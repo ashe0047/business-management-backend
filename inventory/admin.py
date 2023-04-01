4551ee9d45aa65af16d9c90806734c8641dc8ac8
Aw <awshengxiang@hotmail.com>
2023-04-01 13:01:29 +0800

    updated db table name

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from inventory.models import *
 
 # Register your models here.
-admin.site.register(Products)
-admin.site.register(ProductsSupplier)
-admin.site.register(Services)
+admin.site.register(Product)
+admin.site.register(ProductSupplier)
+admin.site.register(Service)
--- pos/admin.py
+++ pos/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from pos.models import *
 
 # Register your models here.
-admin.site.register(Sales)
-admin.site.register(SalesItem)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
