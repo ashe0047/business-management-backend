d418dba825e7b1038b93da179e2e584070bab64f
Aw <awshengxiang@hotmail.com>
2023-05-07 14:38:59 +0800

    removed django-extensions from installed apps

--- businessmanagement/settings.py
+++ businessmanagement/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "hrm",
     "inventory",
     "pos",
-    "django_extensions",
+    # "django_extensions",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
